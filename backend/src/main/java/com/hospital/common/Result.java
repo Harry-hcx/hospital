e09f6c15cfe270d7bca2b5d6dad5ff5452e7e61a
GrapeEly <1264059985@qq.com>
2026-07-17 18:10:51 +0800

    fix: add @JsonInclude(NON_NULL) to Result.java, remove null data from responses

--- backend/src/main/java/com/hospital/common/Result.java
+++ backend/src/main/java/com/hospital/common/Result.java
@@ -1,8 +1,10 @@
 package com.hospital.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Result<T> {
     private int code;
     private String message;
